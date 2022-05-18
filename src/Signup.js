c1e42499ca837d48a6953d65e1cb8065a45275ed
AlexMinez <alex_minihan@outlook.com>
2022-05-19 01:22:04 +1000

    Done.

---
+++ src/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  render() {
+    return <div>
+        <input type="password" ></input>
+    </div>
+  }
+}
+
+export default Signup;
